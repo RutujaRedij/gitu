c3781357ad9e9a2420b630c4a1a19ad6ccadbd4e
RutujaRedij <74145646+RutujaRedij@users.noreply.github.com>
2025-07-21 22:06:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
  region = "us-east-1"
- access_key = "AKIAWCZC55UTDV3M65IY"
- secret_key = "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"
 }
 resource "aws_instance" "us-instance" {
  ami = "ami-0453ec754f44f9a4a"
