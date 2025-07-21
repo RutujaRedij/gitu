92798a031083114b61154e4736d09e9cbefc46b0
RutujaRedij <74145646+RutujaRedij@users.noreply.github.com>
2025-07-21 21:43:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
- region = "ap-south-1"
+ region = "us-east-1"
  access_key = "AKIAWCZC55UTDV3M65IY"
  secret_key = "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"
 }
 resource "aws_instance" "us-instance" {
- ami = "ami-04568bde0d686e039"
+ ami = "ami-0453ec754f44f9a4a"
  instance_type = "t2.micro"
  tags = {
   Name = "aws"
