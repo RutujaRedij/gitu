905760d0745db5b074515e539d5f73515522176f
RutujaRedij <74145646+RutujaRedij@users.noreply.github.com>
2025-07-21 20:28:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+ alias = "south"
+ region = "ap-south-1"
+ access_key = "AKIAWCZC55UTDV3M65IY"
+ secret_key = "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"
+}
+resource "aws_instance" "us-instance" {
+ ami = "ami-0453ec754f44f9a4a"
+ instance_type = "t2.micro"
+ tags = {
+  Name = aws
+}
+}
